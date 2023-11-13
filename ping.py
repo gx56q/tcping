5620ee5786be911f90af58b97c309b32dc355578
n1gtm <40951780+gx56q@users.noreply.github.com>
2023-11-13 15:35:32 +0500

    add handling of other TCP flags

--- ping.py
+++ ping.py
@@ -29,15 +29,61 @@ def tcp_ping(target, port, count, timeout, interval, host_type='ipv4'):
             packet = ip_layer / TCP(dport=port, flags="S")
             start_time = time.time()
             reply = sr1(packet, timeout=timeout, verbose=0)
-            if reply and reply[TCP].flags == 0x12:
-                received += 1
-                elapsed = (time.time() - start_time) * 1000
-                times.append(elapsed)
-                print(
-                    f"Reply from {display_target}:{port} time={elapsed:.2f}ms")
+            if reply:
+                if reply.haslayer(TCP):
+                    tcp_flags = reply[TCP].flags
+
+                    if tcp_flags == 0x12:
+                        # SYN-ACK
+                        received += 1
+                        elapsed = (time.time() - start_time) * 1000
+                        times.append(elapsed)
+                        print(
+                            f"Reply from {display_target}:{port} "
+                            f"time={elapsed:.2f}ms")
+                    elif tcp_flags == 0x14:
+                        # RST-ACK
+                        print(f"Port {port} on {display_target} is closed")
+                    elif tcp_flags == 0x11:
+                        # FIN-ACK
+                        print(
+                            f"Port {port} on {display_target} is open, "
+                            f"no response")
+                    elif tcp_flags == 0x10 or tcp_flags == 0x02:
+                        # ACK or SYN
+                        print(
+                            f"Port {port} on {display_target} is open "
+                            f"and reset")
+                    elif tcp_flags == 0x04:
+                        # RST
+                        print(f"Port {port} on {display_target} is closed")
+                    elif tcp_flags & 0x01:
+                        # FIN
+                        print(f"Received FIN from {display_target}:{port}")
+                    elif tcp_flags & 0x11:
+                        # FIN-ACK
+                        print(
+                            f"Received FIN-ACK from {display_target}:{port}")
+                    elif tcp_flags & 0x08:
+                        # PSH
+                        print(f"Received PSH from {display_target}:{port}")
+                    elif tcp_flags & 0x18:
+                        # PSH-ACK
+                        print(
+                            f"Received PSH-ACK from {display_target}:{port}")
+                    else:
+                        print(
+                            f"Received a response from"
+                            f"{display_target}:{port}, "
+                            f"but with unexpected TCP flags: {hex(tcp_flags)}")
+                else:
+                    print(
+                        f"Unexpected response without TCP layer from "
+                        f"{display_target}:{port}")
+
             else:
-                print(
-                    f"No response from {display_target}:{port}")
+                print(f"No response from {display_target}:{port}")
+
             sent += 1
             if interval:
                 time.sleep(interval)
